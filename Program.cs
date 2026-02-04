cd16b0baacf633dfb2db6ca39326aa45a346780f
Timothyyy24 <s10268547@connect.np.edu.sg>
2026-02-04 23:42:37 +0800

    .

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
 ﻿using FirasTimothy_PRG2Assignment;
 string[] restaurantfile = File.ReadAllLines("restaurants.csv");
 
-for(int i = 1; i<restaurantfile.Length; i++)
-{
-    string lines[] = restaurantfile[i].Split(",");
-
-}
 
 string[] fooditemsfile = File.ReadAllLines("fooditems.csv");
 
--- obj/Debug/net8.0/FirasTimothy_PRG2Assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirasTimothy_PRG2Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48dc1007b633ed885d086c8240bb1462811abe73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ec3c61f93b6f2d4584339e3967685c812087394")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
