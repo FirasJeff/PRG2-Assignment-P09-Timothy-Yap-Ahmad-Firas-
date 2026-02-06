d69f7c73f5f45e088fd8e23d4606171b11d60c37
Timothyyy24 <s10268547@connect.np.edu.sg>
2026-02-06 18:45:25 +0800

    .

--- obj/Debug/net8.0/FirasTimothy_PRG2Assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/FirasTimothy_PRG2Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664471a1ab70c1f8d7325c09615b30060257ddee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abee4c951d145f62c55e70b201b31a97a7983e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
