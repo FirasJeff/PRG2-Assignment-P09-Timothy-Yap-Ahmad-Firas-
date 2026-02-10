45a893700cebeb3a3915ca8f76747557687c70e6
Timothyyy24 <s10268547@connect.np.edu.sg>
2026-02-10 23:31:18 +0800

    idk whats gg on

--- obj/Release/net8.0/FirasTimothy_PRG2Assignment.AssemblyInfo.cs
+++ obj/Release/net8.0/FirasTimothy_PRG2Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195a7d444b2f3808048ca36cd70b53b3a82bb9b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d03d71205b476e4c4f4109918a85c3af3342c82")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirasTimothy_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
